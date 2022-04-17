9c605827c824a6a4142641cd812c382099fe8b71
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-16 22:08:50 -0400

    add stored procedure

--- queries/sp_getAllMovies.sql
+++ queries/sp_getAllMovies.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE `sp_getAllMovies`()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_getAllMovies`()
 BEGIN
 	SELECT DISTINCT name, production_date, mpaa_rating, movie_length_in_mins, movie_poster_url, summary, visual_type
 		FROM movie;
---
+++ queries/sp_getAllShowingMoviesWithAvail.sql
@@ -0,0 +1,31 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_getAllShowingMoviesWithAvail`(IN beforeDate DATETIME, IN afterDate DATETIME)
+BEGIN
+    IF beforeDate IS NULL AND afterDate IS NULL THEN
+        SELECT DISTINCT m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+			FROM movie m 
+			JOIN movie_showing ms ON m.movie_id = ms.movie_id
+            GROUP BY m.movie_id, m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+            HAVING SUM(ms.num_avail_seats) != 0;
+    ELSEIF beforeDate IS NOT NULL AND afterDate IS NULL THEN
+        SELECT DISTINCT m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+			FROM movie m 
+			JOIN movie_showing ms ON m.movie_id = ms.movie_id
+            WHERE ms.show_datetime < beforeDate
+            GROUP BY m.movie_id, m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+            HAVING SUM(ms.num_avail_seats) != 0;
+    ELSEIF beforeDate IS NULL AND afterDate IS NOT NULL THEN
+        SELECT DISTINCT m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+			FROM movie m 
+			JOIN movie_showing ms ON m.movie_id = ms.movie_id
+            WHERE ms.show_datetime > afterDate
+            GROUP BY m.movie_id, m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+            HAVING SUM(ms.num_avail_seats) != 0;
+    ELSE
+        SELECT DISTINCT m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+			FROM movie m 
+			JOIN movie_showing ms ON m.movie_id = ms.movie_id
+            WHERE ms.show_datetime BETWEEN beforeDate AND afterDate
+            GROUP BY m.movie_id, m.name, m.production_date, m.mpaa_rating, m.movie_length_in_mins, m.movie_poster_url, m.summary, m.visual_type
+            HAVING SUM(ms.num_avail_seats) != 0;
+    END IF;
+END

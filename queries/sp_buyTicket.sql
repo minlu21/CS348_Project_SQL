65c698f1732c10bdcd5fb2f8e1a4f535b47d1ef6
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-16 22:40:16 -0400

    edited sp_buyTicket

--- queries/sp_buyTicket.sql
+++ queries/sp_buyTicket.sql
@@ -1,21 +1,20 @@
 DROP PROCEDURE IF EXISTS sp_buyTicket;
 DELIMITER //  
-CREATE PROCEDURE sp_buyTicket(IN movieName VARCHAR(255), IN roomId INT, IN showDatetime DATETIME, IN seatNum INT, IN userId BIGINT, OUT exit_code INT)
+CREATE PROCEDURE sp_buyTicket(IN movieId BIGINT, IN roomId INT, IN showDatetime DATETIME, IN seatNum INT, IN userId BIGINT, OUT exit_code INT)
 BEGIN
 
     DECLARE avail INT;
-    DECLARE movieId BIGINT;
     DECLARE insertPrice DOUBLE;
     DECLARE curr_time DATETIME;
     DECLARE isAvail TINYINT(1) DEFAULT false; 
 
     SELECT NOW() INTO curr_time;
 
-    SELECT num_avail_seats, price, m.movie_id
-        INTO avail, insertPrice, movieId
+    SELECT num_avail_seats, price
+        INTO avail, insertPrice
         FROM movie_showing ms
         JOIN movie m ON ms.movie_id = m.movie_id
-        WHERE ms.room_id = roomId AND m.name = movieName AND ms.show_datetime = showDatetime;
+        WHERE ms.room_id = roomId AND m.movie_id = movieId AND ms.show_datetime = showDatetime;
 
     IF avail > 0 THEN
         -- check if seat is available --
@@ -32,9 +31,11 @@ BEGIN
     ELSE
         SELECT 1 INTO exit_code;
     END IF;
+    
 END //
 DELIMITER ;
 
+-- USE MOVIE ID instead of MOVIE NAME for FIRST PARAMETER:
 -- call sp_buyTicket("Harry Potter and the Philosopher's Stone", 1, '2022-11-04 20:00:00', 15, 1, @exit);
 -- call sp_buyTicket("Harry Potter and the Philosopher's Stone", 1, '2022-11-04 20:00:00', 15, 1, @exit);
 -- call sp_buyTicket("Harry Potter and the Philosopher's Stone", 1, '2022-11-04 20:00:00', 2, 1, @exit);
